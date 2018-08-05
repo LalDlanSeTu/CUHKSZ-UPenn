929d89ce879ae2f513bb9cbcd4cccf385a7b2338
RNGtheRang <1530525787@qq.com>
2018-08-06 00:51:21 +0800

    roConSailboat

---
+++ Python-Sailboat/hello.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python 
+# -*- coding: UTF-8 -*-
+a=10
+
+if a<6:
+    print("True")
+else:
+    print("False")
--- duoji2/duoji2.ino
+++ duoji2/duoji2.ino
@@ -3,25 +3,36 @@
 #include <JY901.h>
 
 Servo myservo;  // create servo object to control a servo
+Servo fan;
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
+int pos_fan = 0;
+int number;
 
 void setup() {
-    Serial.begin(57600);
+  Serial.begin(57600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  fan.attach(10);
   JY901.StartIIC();
 }
 
-
 void loop() {
-      JY901.GetAngle();
-    pos=(float)JY901.stcAngle.Angle[2]/32768*180;
-    if(pos < 0){
-      pos=-pos;
+    JY901.GetAngle();
+    Serial.println((float)JY901.stcAngle.Angle[2]/32768*180);
+    char putout;
+    putout=Serial.read();
+    if(putout=='Z'){
+       pos=Serial.parseInt();
+       myservo.write(pos);
     }
-    myservo.write(pos);
+    if(putout == 'F'){
+       pos_fan=Serial.parseInt();
+       fan.write(pos_fan);
+    }
+    Serial.println("duoji pos: ");
     Serial.println(pos);
-    delay(15); 
-
+    Serial.println("fan pos: ");
+    Serial.println(pos_fan);
+    delay(20); 
 }
